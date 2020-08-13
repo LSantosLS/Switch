be5a452d5655db0139c14d19ade01ed0df41dca4
Luis Santos <lsantos.ls@outlook.com>
2020-08-13 11:20:51 -0300

    Finalização da seção 3

---
+++ Switch.Domain/Entities/StatusRelacionamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Entities
+{
+    public class StatusRelacionamento
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
---
+++ Switch.Infra.Data/Migrations/20200813015649_AdicionandoUsuarioConfigurationv3.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Switch.Infra.Data.Migrations
+{
+    public partial class AdicionandoUsuarioConfigurationv3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Usuarios",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
